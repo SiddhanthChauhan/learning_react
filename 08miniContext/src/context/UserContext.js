57a19256b3707e94c688c3da25c264ab0c8a96d3
SiddhanthChauhan <siddhanth.chauhan7@gmail.com>
2025-09-17 15:41:33 +0530

    Context API discussion

---
+++ 08miniContext/src/context/UserContext.js
@@ -0,0 +1,6 @@
+import React from 'react';
+
+const UserContext = React.createContext();
+//a way to share data globally across components without having to pass props manually at every level.
+
+export default UserContext;
